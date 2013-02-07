ab66a621b4c4dec8945ab1f880d2d9e272c03702
Alan Hale III <alan.hale@geneva.edu>
2013-02-07 00:25:53 -0500

    Added encoder library files. adhale

---
+++ PIC Code/GCR - Encoder Module.X/enclib.h
@@ -0,0 +1,34 @@
+/* 
+ * File:   enclib.h
+ * Author: Alan
+ *
+ * Created on February 6, 2013, 9:12 PM
+ */
+
+#ifndef ENCLIB_H
+#define	ENCLIB_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    //Requirement:  library should be able to measure distance traveled
+    //  (use TMR1; measure cycles, heretofore called COUNTS)
+    // **This function returns the current value of TMR0
+
+
+
+
+
+
+
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ENCLIB_H */
+
---
+++ PIC Code/GCR - Encoder Module.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=GCR_-_Encoder_Module.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/GCR_-_Encoder_Module.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=gcr-encodermodule.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/gcr-encodermodule.x.tar
